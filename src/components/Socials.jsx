adeadc31d9017078fdabb15a2e7be1e8d3455c40
devemit <i.mitko@outlook.com>
2023-02-01 20:58:31 +0100

    cv fix

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -14,9 +14,10 @@ const Socials = () => {
       <a
         target="_blank"
         href="MitkoCV.pdf"
+        rel="noreferrer"
         className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
       >
-        Resume
+        <span>Resume</span>
       </a>
     </div>
   );
