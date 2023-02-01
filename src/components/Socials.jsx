427c71a04aa77a7dee411343aee0f1447205ee22
devemit <i.mitko@outlook.com>
2023-02-01 22:17:12 +0100

    final

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -11,14 +11,14 @@ const Socials = () => {
       <a target="_blank" href="https://github.com/devemit">
         <GrGithub size={35} className="mr-8 hover:text-orange-500" />
       </a>
-      <a
+      {/* <a
         href="mitkocv.pdf"
         target="_blank"
         rel="noreferrer"
         className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
       >
         <span>Resume</span>
-      </a>
+      </a> */}
     </div>
   );
 };
