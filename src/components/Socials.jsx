643a2ed3aff02f52212ef0f276c44f9696459871
devemit <i.mitko@outlook.com>
2023-02-23 23:03:15 +0100

    remove

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -11,15 +11,6 @@ const Socials = () => {
       <a target="_blank" href="https://github.com/devemit">
         <GrGithub size={35} className="mr-8 hover:text-orange-500" />
       </a>
-      <a
-        href="mitkoCV.pdf"
-        target="_blank"
-        download
-        rel="noreferrer"
-        className="text-gray-900 bg-transparent border border-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none font-medium rounded-lg text-base px-5 py-1.5 mr-2 mb-2"
-      >
-        <span>Resume</span>
-      </a>
     </div>
   );
 };
